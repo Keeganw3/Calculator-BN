ee5d5f7a74c269c7f1b4445c6c53d0ada815b464
Keeganw3 <keeganw749@gmail.com>
2023-03-16 00:39:20 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const plus = document.querySelector('#plus')
+const minus = document.querySelector('#minus')
+const multiply = document.querySelector('#multiply')
+const divide = document.querySelector('#divide')
+
+function operate(x,y) {
+    const param1 = 1;
+    const param2 = 2;
+    if {(plus.addEventListener ('click', function (e) {
+        console.log('add')
+    })};
+    else {
+        console.log('X')
+    }
+}
+
+function add() {
+    
+    return
+}
